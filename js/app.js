c73a2f3b8d6a9424b5968363cee2b2126ae382b4
Jennifer Enomoto-Hahn <hugomcmittens@Jennifers-Air.home>
2019-02-02 17:17:03 -1000

    left off on starship films. need to finish the XMLhttpRequest.

--- js/app.js
+++ js/app.js
@@ -14,6 +14,9 @@ const getButton = document.getElementById("requestResourceButton");
 const displayHTML = () => {
   const getDropDownValue = document.getElementById("resourceType").value;
   const getResource = document.getElementById("resourceId").value;
+  while (getContainer.firstChild) {
+    getContainer.removeChild(getContainer.firstChild);
+  }
   if (getDropDownValue === "people") {
     const peopleReq = new XMLHttpRequest();
     peopleReq.addEventListener("load", function() {
@@ -49,14 +52,52 @@ const displayHTML = () => {
       planetData.forEach(x => {
         if (x.name === getResource) {
           const ptName = document.createElement("h2");
-          ptName.innerHTML = x.name;
+          ptName.innerHTML = "Plant Name: " + x.name;
           getContainer.appendChild(ptName);
+          const ptTerr = document.createElement("p");
+          ptTerr.innerHTML = "Terrain: " + x.terrain;
+          getContainer.appendChild(ptTerr);
+          const ptPop = document.createElement("p");
+          ptPop.innerHTML = "Population: " + x.population;
+          getContainer.appendChild(ptPop);
         }
       });
     });
     planetReq.open("GET", "https://swapi.co/api/planets");
     planetReq.send();
+  } else if (getDropDownValue === "starships") {
+    const starShipReq = new XMLHttpRequest();
+    starShipReq.addEventListener("load", function() {
+      const starShipData = JSON.parse(this.responseText).results;
+      console.log(starShipData);
+      starShipData.forEach(x => {
+        if (x.name === getResource) {
+          const shipName = document.createElement("h2");
+          shipName.innerHTML = "Starship Name: " + x.name;
+          getContainer.appendChild(shipName);
+          const manufac = document.createElement("p");
+          manufac.innerHTML = "Manufacturer: " + x.manufacturer;
+          getContainer.appendChild(manufac);
+          const starClass = document.createElement("p");
+          starClass.innerHTML = "Starship Class: " + x.starship_class;
+          getContainer.appendChild(starClass);
+          console.log(x.films);
+          x.films.forEach(x => {
+            const starFilm = new XMLHttpRequest();
+          });
+          //   const ulFilm = document.createElement('ul');
+          //   starShipData.forEach(x => {
+          //       const shpfilmNames = new XMLHttpRequest();
+          //       shpfilmNames.addEventListener('load', function(){
+
+          //       })
+          //       shpfilmNames.open('GET', starShipData.film)
+          //   })
+        }
+      });
+    });
+    starShipReq.open("GET", "https://swapi.co/api/starships");
+    starShipReq.send();
   }
 };
-
 getButton.addEventListener("click", displayHTML);
